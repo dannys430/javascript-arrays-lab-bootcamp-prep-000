d9e890460b9e6817a1a829f433f65d0829ce81c6
Daniel Singer <dannys430@yahoo.com>
2019-07-05 16:50:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten() {
+  kittens.push("Ralph");
+}
+
+function destructivelyPrependKitten() {
+  kittens.unshift("Bob");
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  kittens.concat("Broom");
+}
+
+
+
+//define your array here
 
 // Add your functions and code here
