b212e1485a532c364d24d663a98198d9cc1ca400
Daniel Singer <dannys430@yahoo.com>
2019-07-05 17:12:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,6 +28,9 @@ function removeLastKitten() {
   return kittens.slice(0, kittens.length - 1);
 }
 
+function removeFirstKitten() {
+  return kittens.slice(1, kittens.length);
+}
 //define your array here
 
 // Add your functions and code here
