2e79784c1d991511675034d2490e3f21df1cfdea
Daniel Singer <dannys430@yahoo.com>
2019-07-05 17:10:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  kittens.slice(0, kittens.length - 1);
+  return kittens.slice(0, kittens.length - 1);
 }
 
 //define your array here
